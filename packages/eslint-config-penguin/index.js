863a6a7a4a8c89c5a27198983c59246087494c1a
GalvinGao <me@galvingao.com>
2022-12-01 17:08:32 -0500

    styles: unify eslint config

--- packages/eslint-config-penguin/index.js
+++ packages/eslint-config-penguin/index.js
@@ -4,10 +4,13 @@ module.exports = {
     'prettier',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'plugin:json/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
     'no-unused-vars': 'off',
     eqeqeq: 'error',
   },
+  parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier', 'import', 'turbo'],
 }
