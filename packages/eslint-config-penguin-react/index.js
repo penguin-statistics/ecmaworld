a83603e0f86b947f5e89d3b14be0914b1fe6b623
GalvinGao <me@galvingao.com>
2023-07-22 21:38:09 +0800

    style: cleanup

--- packages/eslint-config-penguin-react/index.js
+++ packages/eslint-config-penguin-react/index.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['penguin', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
   plugins: ['jsx-a11y'],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
     'react/prop-types': 'off',
   },
   settings: {
